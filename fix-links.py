5f11bd9d7a37e7a979b856abdc377ff10f563600
Jouni Leino <jouni.leino@siili.com>
2021-11-10 21:59:09 +0100

    Adding instructions to script

--- fix-links.py
+++ fix-links.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 from bs4 import BeautifulSoup
 
+"""
+Before using this script run the following command: 
+npm run dist
+
+Hosting a static website in AWS S3 requires that names of the accessed object is exactly the one that a link points to.
+Astro creates a /dist directory with all the HTML pages. Astro creates also routes that have no .html extension.
+For example a link in Astro site points to /contact and the /dist directory contains a file named contact.html.
+In order to access that HTML page in a S3 bucket the link needs to point to a path with the .html extension.
+This script loops through the specified HTML files and adds the .html extension to links.
+
+for example this 
+<a href="/contact">Link</a>
+becomes this
+<a href="/contact.html">Link</a>
+
+This script will update the links and write the prettified html content into each HTML file.
+After this script has complete you can sync the /dist directory content to S3 bucket.
+"""
+
 files = ["dist/index.html", "dist/contact.html"]
 
 for file in files:
